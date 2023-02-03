3457daf9921d20a961a5fa5d7436b06510980968
Luca Mariani <luca.mariani@ext.esa.int>
2023-02-03 17:12:06 +0100

    Update common.py
    Add file name patterns and file format standard versions.

--- eopfiles/common.py
+++ eopfiles/common.py
@@ -1,1 +1,13 @@
 """Common objects."""
+import enum
+
+
+FILE_NAME_PATTERN_FFS1 = r"([A-Z_]){2}_([A-Z0-9_]){4}_([A-Z0-9_]){10}_([A-Z0-9_]){1,41}"
+FILE_NAME_PATTERN_FFS2 = r"([A-Z0-9_]){3}_([A-Z0-9_]){4}_([A-Z0-9_]){10}_([A-Z0-9_]){1,41}"
+FILE_NAME_PATTERN_FFS3 = FILE_NAME_PATTERN_FFS2
+
+
+class FFSVersion(enum.Enum):
+    FFS1 = "1.0"
+    FFS2 = "2.0"
+    FFS3 = "3.0"
