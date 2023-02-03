88ea8d9eae34c1106aa2498f94810c176a5fd11e
Luca Mariani <luca.mariani@ext.esa.int>
2023-02-03 17:20:49 +0100

    File name patterns moved to basic.py

---
+++ eopfiles/basic.py
@@ -0,0 +1,5 @@
+"""Basic types."""
+
+FILE_NAME_PATTERN_FFS1 = r"([A-Z_]){2}_([A-Z0-9_]){4}_([A-Z0-9_]){10}_([A-Z0-9_]){1,41}"
+FILE_NAME_PATTERN_FFS2 = r"([A-Z0-9_]){3}_([A-Z0-9_]){4}_([A-Z0-9_]){10}_([A-Z0-9_]){1,41}"
+FILE_NAME_PATTERN_FFS3 = FILE_NAME_PATTERN_FFS2
