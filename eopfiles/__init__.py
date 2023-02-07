5f70d354fa7c7f36e28b4014e15f83dd0a636a29
Luca Mariani <luca.mariani@ext.esa.int>
2023-02-07 18:47:14 +0100

    Add parser and serializer

--- eopfiles/__init__.py
+++ eopfiles/__init__.py
@@ -1,3 +1,8 @@
+from xsdata.formats.dataclass.context import XmlContext
+from xsdata.formats.dataclass.parsers import XmlParser
+from xsdata.formats.dataclass.serializers import XmlSerializer
+from xsdata.formats.dataclass.serializers.config import SerializerConfig
+
 from . import (
     aux_orbres,
     basic,
@@ -6,11 +11,18 @@ from . import (
     times
 )
 
+
+context = XmlContext()
+parser = XmlParser(context=context)
+serializer = XmlSerializer(context=context, config=SerializerConfig(pretty_print=True))
+
 __all__ = [
     "aux_orbres",
     "basic",
     "headers",
     "mpl_orbpre",
+    "parser",
+    "serializer",
     "times"
 ]
 
